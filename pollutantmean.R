9e49a04b2c8fe2405dc1b75154228392ca140ac1
gsar4014 <gksarathy@gmail.com>
2021-06-23 20:30:24 -0500

    Edited pollutantmean function to remove missing values from the dataset.

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,5 +2,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
   pollutantfiles <- dir(paste(directory), full.names = TRUE)[id]
   pollutantdata <- lapply(pollutantfiles, function(x) read.csv(x)[pollutant])
   pollutantlist <- unlist(pollutantdata)
-  mean(pollutantlist)
+  missingvalues <- is.na(pollutantlist)
+  pollutantvalues <- pollutantlist[!missingvalues]
+  mean(pollutantvalues)
 }
