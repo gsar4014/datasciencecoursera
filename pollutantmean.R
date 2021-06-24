a8809cc741c66d8f87dbdd7c09ac36352a526924
gsar4014 <gksarathy@gmail.com>
2021-06-23 20:22:25 -0500

    Created first iteration of pollutantmean function.  Hope it works.

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,6 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
-  
+  pollutantfiles <- dir(paste(directory), full.names = TRUE)[id]
+  pollutantdata <- lapply(pollutantfiles, function(x) read.csv(x)[pollutant])
+  pollutantlist <- unlist(pollutantdata)
+  mean(pollutantlist)
 }
