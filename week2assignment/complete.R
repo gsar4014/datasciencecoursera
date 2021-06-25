b099ec658ab6b013f25fec418a5518975241d825
gsar4014 <gksarathy@gmail.com>
2021-06-24 20:30:25 -0500

    Preliminary version of complete function.  Results are mathematically correct, but are not delivered in clean data table format.

--- week2assignment/complete.R
+++ week2assignment/complete.R
@@ -1,3 +1,7 @@
 complete <- function(directory, id = 1:332){
-  
+  pollutantfiles <- dir(paste(directory), full.names = TRUE)[id]
+  completedatafiles <- lapply(pollutantfiles, function(x) 
+    complete.cases(read.csv(x)))
+  completedatatable <- lapply(completedatafiles, function(x) sum(unlist(x)))
+  completedatatable
 }
