f6cca017c7b14cf711efcf1ed5ee1d6ba77381fa
gsar4014 <gksarathy@gmail.com>
2021-06-25 01:22:26 -0500

    Completed function with a cleaned up data frame format with column named "nobs" for number of complete observations, and rows named for each spec observed.

--- week2assignment/complete.R
+++ week2assignment/complete.R
@@ -2,6 +2,11 @@ complete <- function(directory, id = 1:332){
   pollutantfiles <- dir(paste(directory), full.names = TRUE)[id]
   completedatafiles <- lapply(pollutantfiles, function(x) 
     complete.cases(read.csv(x)))
-  completedatatable <- lapply(completedatafiles, function(x) sum(unlist(x)))
+  completedata <- lapply(completedatafiles, function(x) sum(unlist(x)))
+  completedata
+  completedatatable <- data.frame(completedata)
+  completedatatable <- t(completedatatable)
+  row.names(completedatatable) <- id
+  colnames(completedatatable) <- "nobs"
   completedatatable
 }
