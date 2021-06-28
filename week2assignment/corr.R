2890efe4a758e5f502b13beb89dd9d08d1fa4981
gsar4014 <gksarathy@gmail.com>
2021-06-27 19:21:56 -0500

    Had to edit threshold_datafiles name to be consistent and not return error.

--- week2assignment/corr.R
+++ week2assignment/corr.R
@@ -1,9 +1,9 @@
 corr <- function(directory, threshold = 0){
   completedataframe <- complete(directory)
   thresholddataframe <- completedataframe[completedataframe$nobs > threshold,]
-  thesholddatafiles <- dir(paste(directory), full.names = TRUE)[
+  threshold_datafiles <- dir(paste(directory), full.names = TRUE)[
     thresholddataframe$id]
-  thresholddataframes <- lapply(thresholddatafiles, function(x) 
+  thresholddataframes <- lapply(threshold_datafiles, function(x) 
     read.csv(x))
   correlations_vector <- lapply(thresholddataframes, function(x)
     corr(unlist(x)))
