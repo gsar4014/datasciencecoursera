af9f420c7e589da49942b50c107ac47eebaeab0a
gsar4014 <gksarathy@gmail.com>
2021-06-30 16:20:45 -0500

    Completed corr function for real with product of integer vector of correlations between pollutant measures in each sensor.

--- week2assignment/corr.R
+++ week2assignment/corr.R
@@ -3,10 +3,13 @@ corr <- function(directory, threshold = 0){
   thresholddataframe <- completedataframe[completedataframe$nobs > threshold,]
   threshold_datafiles <- dir(paste(directory), full.names = TRUE)[
     thresholddataframe$id]
-  thresholddataframes <- lapply(threshold_datafiles, function(x) 
+  thresholddatasensors <- lapply(threshold_datafiles, function(x) 
     read.csv(x))
-  correlations_vector <- lapply(thresholddataframes, function(x)
-    corr(unlist(x)))
+  threshold_data_frames <- lapply(thresholddatasensors, function(x)
+    data.frame(x))
+  correlations_vector <- lapply(threshold_data_frames, function(x)
+    cor(x$nitrate, y = x$sulfate, use = "complete.obs"))
+  correlations_vector <- unlist(correlations_vector)
   correlations_vector
   
 }
